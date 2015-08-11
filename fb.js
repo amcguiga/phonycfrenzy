fe9203824560c67c2b8eced068cd39e630c6833b
Alex <amcguiga@gmail.com>
2015-08-11 18:14:39 -0400

    initial stuff

---
+++ fb.js
@@ -0,0 +1,100 @@
+var evade ={
+	'def': '(v.)  - to dodge/avoid',
+	'tanslation': {
+		'spanish': 'evadir',
+        'english':'evade'
+	}
+};
+
+var feign ={
+	'def': '(v.) - pretend',
+	'translation': {
+		'spanish': 'fingir',
+        'english':'feign'
+	}
+}; 
+
+var grim ={
+	'def': '(adj.) - serious',
+	'translation':{
+		'spanish':'serio(a)',
+        'english':'grim'
+	}
+};
+
+var exalt ={
+	'def': '(v.) - to praise',
+	'translation':{
+		'spanish': 'exaltar',
+        'english':'exalt'
+	}
+};
+
+var fetid ={
+	'def': '(adj.) - malodorous',
+	'translation':{
+		'spanish': 'fétido(a)',
+        'english':'fetid'
+	}
+};
+
+var filch ={
+	'def': '(v.) - to steal',
+	'translation':{
+		'spanish': 'robar',
+        'english':'filch'
+	}
+};
+
+var flair ={
+	'def': '(n.) - talent',
+	'translation':{
+		'spanish': 'talento',
+        'english':'flair'
+	}
+};
+
+var flout ={
+	'def': '(v.) - comtempt',
+	'translation':{
+		'spanish': 'desobeder(reglas o autoridad)',
+        'english':'flout'
+	}
+};
+
+var foil ={
+	'def': '(v.) - defeat',
+	'translation':{
+		'spanish': 'desbaratar los planes de',
+        'english': 'foil'
+	}
+};
+
+var forgo ={
+	'def': '(v.) - give up',
+	'translation':{
+		'spanish': 'abstenerse',
+        'english':'forgo'
+	}
+};
+
+var glut ={
+	'def': '(n.) - fill to excess',
+	'translation':{
+		'spanish': 'exceso(m)/ superabundancia(f)',
+        'english':'glut'
+	}
+};
+
+var guile ={
+	'def': '(n.) - deceit',
+	'translation':{
+		'spanish': 'astucia',
+        'english': 'guile'
+	}
+};
+
+var words= [guile, evade, feign, grim, exalt, fetid];
+
+
+
